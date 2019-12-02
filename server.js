700e0416cc71a76eb75a21a4a3c752ed1065b960
Tim Golding <tgolding55@hotmail.com>
2019-12-02 11:50:48 +0000

    connected to front end

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/callback", function(req, res) {
   };
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token;
-    let uri = process.env.FRONTEND_URI || "http://localhost:3000";
+    let uri = process.env.FRONTEND_URI || "http://localhost:3000/auth";
     res.redirect(uri + "?access_token=" + access_token);
   });
 });
